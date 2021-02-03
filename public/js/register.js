a820112ec130506882cc1bad64ada93bba0ad5ec
ralfhei <ralf.heinsoo@gmail.com>
2021-02-03 13:40:24 +0200

    fix-validate-error

--- public/js/register.js
+++ public/js/register.js
@@ -1,11 +1,11 @@
-import DefaultExport from 'register-validate.js';
 const registerButton = document.getElementById('register-button');
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 
 registerButton.addEventListener('click', e => {
-    console.log(314);
-    const data = { username: usernameInput.value, password: passwordInput.value };
+    if(this.validate(e) == true) {
+
+    const data = {username: usernameInput.value, password: passwordInput.value};
 
     fetch('/register', {
         method: 'POST',
@@ -14,17 +14,54 @@ registerButton.addEventListener('click', e => {
         },
         body: JSON.stringify(data),
     })
-        .then(res => res.json())
-        .then(data => {
-            if (data.response == 'success') {
-                window.location.href = "/";
-            }
-            else if (data.response == "errorUserExists") {
-                $("#register-error").textContent("User already exists!");
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+    .then(res => res.json())
+    .then(data => {
+        if ( data.response == 'success' ) {
+            window.location.href = "/";
+        } else if (data == "errorUserExists") {
+            $("#register-error").textContent("User already exists!");
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
+}
 });
+
+function validate(e) {
+    e.preventDefault();
+  
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const passwordAgain = document.getElementById("password-again").value;
+  
+    if (!username || username === '' || username.length < 6) {
+      const nameError = document.getElementById("username-error");
+      nameError.innerHTML = `
+          <p>Username not valid!</p>
+      `
+      return false
+    }
+  
+    if(password.length < 6 || !password) {
+      const passwordError = document.getElementById("password-error");
+      passwordError.innerHTML = `
+          <p>Password not valid!</p>
+      `
+      return false
+    }
+  
+    if(password !== passwordAgain) {
+      const passwordAgainError = document.getElementById("password-again-error");
+      passwordAgainError.innerHTML = `
+          <p>Password must be the same!</p>
+      `
+      return false
+    }
+    else{
+        console.log("yooo")
+        return true
+    }
+  }
+  
